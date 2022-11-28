bad5c46a64f170d1f7690bc892c8966ce1d240d9
sollyu <sollyu@qq.com>
2022-11-28 13:53:00 +0800

    :sparkles: add common.function.sh

---
+++ common.function.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#####################[颜色-开始]######################
+COLOR_RED="\033[31m"
+COLOR_GREEN="\033[32m"
+COLOR_YELLOW="\033[33m"
+COLOR_BLUE="\033[34m"
+COLOR_END="\033[0m"
+#####################[颜色-结束]######################
+
+function test() {
+    echo "test call ing."
+}
